6feb42f40cc492837b0b37a4f4963455efd93605
KayanIbraHem <kioghubacc@gmail.com>
2025-07-23 14:23:05 +0300

    update

--- auth/controller/auth_controller.js
+++ auth/controller/auth_controller.js
@@ -13,6 +13,24 @@ const AuthRegister = (app)=>{
         AuthData.save();
         res.json(DataStatus);
     });
+
+   app.get('/get_users', async (req, res) => {
+        try {
+            const Data = await Auth.find(); 
+            const DataStatus = {
+                status: true,
+                message: "Fetch Successfully",
+                data: Data
+            }
+            res.json(DataStatus);
+        } catch (error) {
+            res.status(500).json({
+                status: false,
+                message: "Error fetching data",
+                error: error.message
+            });
+        }
+    });
 }
 
 export default AuthRegister
