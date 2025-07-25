4a39bd8d5b26a4090372f673f586a7b141f3593d
MohabMohamed72 <mohabmohamedd72@gmail.com>
2025-07-25 15:10:42 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import AuthRegister from "./auth/controller/auth_controller.js";
 import StartServer from './mongooseConnection.js'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors(
+    {
+        origin: 'https://yourfrontend.com', // Allow only this domain
+        // or multiple domains:
+        // origin: ['https://yourfrontend.com', 'http://localhost:3000'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+        allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+        credentials: true, // Enable if using cookies/auth
+    }
+))
 StartServer.apply(this);
 AuthRegister(app);
 
--- mongooseConnection.js
+++ mongooseConnection.js
@@ -4,7 +4,8 @@ import Password from "./mongoPassword.js";
 const StartServer = ()=>{
     mongoose
       .connect(
-        `mongodb+srv://bhoba17:${Password}@cluster0.6ioz53q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`
+`mongodb+srv://bhoba17:${Password}@cluster0.6ioz53q.mongodb.net/AuthApi?retryWrites=true&w=majority&appName=Cluster`
+        // `mongodb+srv://bhoba17:${Password}@cluster0.6ioz53q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`
       )
       .then(() => {
         console.log("success");
